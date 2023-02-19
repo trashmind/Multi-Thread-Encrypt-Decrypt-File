df325dc1b112da9636b53a380dd248f4a4c7ed5e
Trash Mind <45304629+trashmind@users.noreply.github.com>
2023-02-19 19:45:49 +0700

    beta version

---
+++ Multi-Thread Encrypt Decrypt File/Multi-Thread Encrypt Decrypt File/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.L1 = New System.Windows.Forms.ListBox()
+        Me.BGWMain = New System.ComponentModel.BackgroundWorker()
+        Me.BtnEcDc = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'L1
+        '
+        Me.L1.AllowDrop = True
+        Me.L1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.L1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.L1.ForeColor = System.Drawing.Color.White
+        Me.L1.FormattingEnabled = True
+        Me.L1.Location = New System.Drawing.Point(0, 0)
+        Me.L1.Name = "L1"
+        Me.L1.Size = New System.Drawing.Size(337, 263)
+        Me.L1.TabIndex = 0
+        '
+        'BGWMain
+        '
+        '
+        'BtnEcDc
+        '
+        Me.BtnEcDc.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnEcDc.Location = New System.Drawing.Point(260, 3)
+        Me.BtnEcDc.Name = "BtnEcDc"
+        Me.BtnEcDc.Size = New System.Drawing.Size(74, 38)
+        Me.BtnEcDc.TabIndex = 1
+        Me.BtnEcDc.Text = "Encrypt"
+        Me.BtnEcDc.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(337, 263)
+        Me.Controls.Add(Me.BtnEcDc)
+        Me.Controls.Add(Me.L1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Multi-Thread Encrypt Decrypt File"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents L1 As System.Windows.Forms.ListBox
+    Friend WithEvents BGWMain As System.ComponentModel.BackgroundWorker
+    Friend WithEvents BtnEcDc As System.Windows.Forms.Button
+
+End Class
---
+++ Multi-Thread Encrypt Decrypt File/Multi-Thread Encrypt Decrypt File/Form1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.IO
+Imports System.Threading
+
+Public Class Form1
+
+    Private Sub FMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Dim aray As String()
+    Private Sub L1_DragDrop(sender As Object, e As DragEventArgs) Handles L1.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            If Not BGWMain.IsBusy Then
+                L1.Items.Clear()
+                aray = e.Data.GetData(DataFormats.FileDrop)
+                BGWMain.RunWorkerAsync()
+            End If
+        End If
+    End Sub
+
+    Private Sub L1_DragEnter(sender As Object, e As DragEventArgs) Handles L1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.All
+        End If
+    End Sub
+
+    Private Sub BGWMain_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BGWMain.DoWork
+        For Each s As String In aray
+            Dim T As New Thread(AddressOf Th) : T.Start(s)
+        Next
+    End Sub
+
+    Private Sub BtnEcDc_Click(sender As Object, e As EventArgs) Handles BtnEcDc.Click
+        If BGWMain.IsBusy Then Return
+        If BtnEcDc.Text = "Encrypt" Then
+            BtnEcDc.Text = "Decrypt"
+        Else
+            BtnEcDc.Text = "Encrypt"
+        End If
+    End Sub
+
+    Private Sub Th(ByVal s As String)
+        Try
+            If BtnEcDc.Text = "Encrypt" Then
+                File.WriteAllBytes(s, EnCryptGen2(File.ReadAllBytes(s)))
+                L1.Items.Add(New FileInfo(s).Name & " (Encrypted By CyberAddict)")
+            Else
+                File.WriteAllBytes(s, DeCryptGen2(File.ReadAllBytes(s)))
+                L1.Items.Add(New FileInfo(s).Name & " (Decrypted By CyberAddict)")
+            End If
+        Catch
+            Try
+                L1.Items.Add(New FileInfo(s).Name & " (Failed)")
+            Catch : End Try
+        End Try
+        Try
+            L1.SelectedIndex = L1.Items.Count - 1
+        Catch : End Try
+    End Sub
+
+
+#Region "Fungsi"
+
+    Private Function EnCryptGen2(ByVal input As Byte(), Optional ByVal key As String = "~{AITGen2}~", Optional ByVal EC As System.Security.Cryptography.RijndaelManaged = Nothing, Optional ByVal SHA256 As System.Security.Cryptography.SHA256Cng = Nothing) As Byte()
+        If EC Is Nothing Then EC = Rij()
+        If SHA256 Is Nothing Then SHA256 = Sha()
+        Try
+            EC.Key = SHA256.ComputeHash(Hasnya(key))
+            EC.Mode = Security.Cryptography.CipherMode.ECB
+            Return EC.CreateEncryptor.TransformFinalBlock(input, 0, input.Length)
+        Catch : End Try
+        Return Nothing
+    End Function
+
+    Private Function DeCryptGen2(ByVal input As Byte(), Optional ByVal key As String = "~{AITGen2}~", Optional ByVal DC As System.Security.Cryptography.RijndaelManaged = Nothing, Optional ByVal SHA256 As System.Security.Cryptography.SHA256Cng = Nothing) As Byte()
+        If DC Is Nothing Then DC = Rij()
+        If SHA256 Is Nothing Then SHA256 = Sha()
+        Try
+            DC.Key = SHA256.ComputeHash(Hasnya(key))
+            DC.Mode = Security.Cryptography.CipherMode.ECB
+            Return DC.CreateDecryptor.TransformFinalBlock(input, 0, input.Length)
+        Catch : End Try
+        Return Nothing
+    End Function
+
+
+
+    Private Function Rij() As System.Security.Cryptography.RijndaelManaged
+        Return New System.Security.Cryptography.RijndaelManaged
+    End Function
+
+    Private Function Sha() As System.Security.Cryptography.SHA256Cng
+        Return New System.Security.Cryptography.SHA256Cng
+    End Function
+
+    Private Function Hasnya(ByVal key As String) As Byte()
+        Return System.Text.ASCIIEncoding.Unicode.GetBytes(key)
+    End Function
+
+#End Region
+
+End Class
